6db144564ddb2c4a027444e77a6c2674c5e5a0e2
Iryna <batsianiryna@gmail.com>
2022-12-19 22:41:32 +0300

    add task4 сумма от 1 до n рекурсия

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+int rec(int n)
+{
+  if (n == 1)
+    return 1;
+  return rec(n - 1) + n;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
+
