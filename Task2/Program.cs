3a346f06b6297320f4ea656b1c0e42ebf16ca4f0
Iryna <batsianiryna@gmail.com>
2022-12-19 19:10:05 +0300

    add task2 факториал числа с помощью рекурсивной функции

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Факториал числа с помощью рекурсивной функции
+
+int factorial(int n)
+{
+  Console.WriteLine($"{n} ");
+  if (n == 0)
+    return 1;
+  return factorial(n - 1) * n;
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(factorial(n));
