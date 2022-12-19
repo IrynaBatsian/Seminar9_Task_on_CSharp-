41a5da4dfcba39bc77bfb16c55c370e0dab02981
Iryna <batsianiryna@gmail.com>
2022-12-19 19:25:46 +0300

    add task3 сумма последовательности чисел

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дана последовательность чисел, завершающаяся числом 0. Найдите сумму всех этих чисел, не используя цикл.
+
+int sum()
+{
+  int n = Convert.ToInt32(Console.ReadLine());
+  if (n == 0)
+     return 0;
+  return sum() + n;
+}
+
+Console.Clear();
+Console.WriteLine(sum());
