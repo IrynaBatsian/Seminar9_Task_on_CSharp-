5759a346e63619dc7b1de17d781d130913f479fc
Iryna <batsianiryna@gmail.com>
2022-12-19 17:59:49 +0300

    add task1 рекурсивная функция для суммы двух чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1 . Также нельзя использовать цикы.
+
+int sum(int n, int m)
+{
+  if (m == 0)
+    return n;
+  return sum(n + 1, m - 1);
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(n, m));
