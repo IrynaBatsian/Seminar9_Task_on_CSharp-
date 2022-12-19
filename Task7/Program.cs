6e30022abab2bb4d6ffc0443200d985dd6644457
Iryna <batsianiryna@gmail.com>
2022-12-19 23:42:49 +0300

    add task7 сумма цифр числа рекурсия

---
+++ Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int rec(int n)
+{
+  if (n % 10 == n || n < 10 || n / 10 ==0) // Можно использовать любой из этих трех базисов. 
+    return n;
+  return rec(n / 10) + n % 10;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
